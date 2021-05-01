b58a07b1c027d4a2fafea0266d0de67985b12cb1
Viktor Lukyanenko <victorluk72@yahoo.ca>
2021-04-30 22:17:40 -0400

    images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -29,6 +29,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Ripo.Home)
 	mux.Get("/about", handlers.Ripo.About)
 
+	//Create file server to manage our static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 
 }
